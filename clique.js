94bc3e44ee37a58e343b75685a0a0232035b9203
Ian Abuaf <abuafi@usi.ch>
2022-05-18 16:35:12 +0200

    Start of GUI

---
+++ clique.js
@@ -0,0 +1,60 @@
+// Size of clique we're trying to find
+var k = 1
+// Number of vertices in the graph
+var instanceVertexCount = 0
+// Adjacency matrix of the graph (only Lower Triangular is required)
+var instance = []
+function setVertexCount(count) {
+    if (count < instanceVertexCount) {
+        console.warn("Reducing vertex count may remove some existing edges");
+        while (instanceVertexCount > count) {
+            instance.pop()
+            instanceVertexCount--
+        }
+    } else {
+        while (instanceVertexCount < count) {
+            instance.push(Array(instanceVertexCount).fill(0))
+            instanceVertexCount++
+        }
+    }
+}
+function addEdge(vertex1, vertex2) {
+    if (vertex1 == vertex2) {
+        throw("Invalid edge")
+    }
+    vs = [vertex1, vertex2].sort()
+    vertex2 = vs[0]
+    vertex1 = vs[1]
+    instance[vertex1][vertex2] = 1
+    return {v1:vertex1, v2:vertex2}
+}
+function solve() {
+    let size = instanceVertexCount;
+    let clauses = []
+    if (size == 0) throw("Graph must contain at least 1 vertex")
+    for (row in instance) {
+        for (col in instance[row]) {
+            // For each possible combination of vertices
+            if (!instance[row][col]) {
+                // If there is no edge create a clause
+                // "Either row or col are not in the clique"
+                clauses.push([-(parseInt(row)+1), -(parseInt(col)+1)])
+            }
+        }
+    }
+    let others = recursiveClauses(size - k + 1)
+    clauses = clauses.concat(others)
+    let solution = satSolve(size, clauses)
+    if (!solution) return solution 
+    return solution.filter(i => i>0).map(i => i-1)
+}
+function recursiveClauses(stop, v = [], j = 0) {
+    let out = []
+    if (v.length == stop) return [v]
+    for (let i = j; i < instanceVertexCount; i++) {
+        next = [...v]
+        next.push(i+1)
+        out = out.concat(recursiveClauses(stop, next, i+1))
+    }
+    return out
+}
